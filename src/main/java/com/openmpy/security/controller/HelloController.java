337f17c228de5e4fc182d1591ff6a5a229c21859
openmpy <suhwan@kakao.com>
2024-07-01 22:19:44 +0900

    chore: Move HelloController to controller package

--- src/main/java/com/openmpy/security/controller/HelloController.java
+++ src/main/java/com/openmpy/security/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.openmpy.security;
+package com.openmpy.security.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
